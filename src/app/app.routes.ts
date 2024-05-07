b24d714ccf4aa46a541d3d47eeebe564b4839e8c
Estefania Rodríguez B <estefania.rguezb21@gmail.com>
2024-05-07 07:17:07 -0600

    Reparar menu, avance de creacion de Préstamos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { VentasComponent } from './ventas/ventas.component';
 import { PrestamosComponent } from './prestamos/prestamos.component';
 import { InventarioComponent } from './inventario/inventario.component';
+import { CreacionPrestamosComponent } from './creacion-prestamos/creacion-prestamos.component';
 export const routes: Routes = [
     {path:"inventario", component:InventarioComponent},
     {path: 'prestamos', component:PrestamosComponent },
-    {path: 'ventas', component:VentasComponent }];
+    {path: 'ventas', component:VentasComponent },
+    {path: 'creacionPrestamos', component:CreacionPrestamosComponent}];
 
---
+++ src/app/creacion-prestamos/creacion-prestamos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creacion-prestamos',
+  standalone: true,
+  imports: [],
+  templateUrl: './creacion-prestamos.component.html',
+  styleUrl: './creacion-prestamos.component.css'
+})
+export class CreacionPrestamosComponent {
+
+}
