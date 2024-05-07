d48e8392f02452a9cf8fe964ac7b36cb7225926f
cuboalcub <fvera8192@gmail.com>
2024-05-07 07:48:13 -0600

    Estructura update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { VentasComponent } from './ventas/ventas.component';
-import { PrestamosComponent } from './prestamos/prestamos.component';
+import { PrestamosComponent } from './Prestamo/prestamos/prestamos.component';
 import { InventarioComponent } from './inventario/inventario.component';
-import { CreacionPrestamosComponent } from './creacion-prestamos/creacion-prestamos.component';
+import { CreacionPrestamosComponent } from './Prestamo/creacion-prestamos/creacion-prestamos.component';
 export const routes: Routes = [
     {path:"inventario", component:InventarioComponent},
     {path: 'prestamos', component:PrestamosComponent },
