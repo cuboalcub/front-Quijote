2fc9a1ff8a859d2b49c3b2fde6d9242da3bfc1bf
FanysSs <estefania.rguezb21@gmail.com>
2024-05-11 22:16:35 -0600

    creacion de registro de empleados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { PrestamosComponent } from './Prestamo/prestamos/prestamos.component';
 import { CreacionPrestamosComponent } from './Prestamo/creacion-prestamos/creacion-prestamos.component';
 import { VentasComponent } from './Venta/ventas/ventas.component';
 import { InventarioComponent } from './Invetarios/inventario/inventario.component';
+import { RegistroEmpleadosComponent } from './registro-empleados/registro-empleados.component';
 
 export const routes: Routes = [
     {path: 'prestamos', component:PrestamosComponent },
     {path: 'creacionPrestamos', component:CreacionPrestamosComponent},
     {path: 'inventario', component:InventarioComponent},
-    {path: 'ventas', component:VentasComponent}];
+    {path: 'ventas', component:VentasComponent},
+    {path: 'registroEmpleados', component:RegistroEmpleadosComponent}];
 
