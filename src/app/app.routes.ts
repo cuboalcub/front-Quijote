9170306b515cd88e4a30819a3c822d0d5915bb6b
FanysSs <estefania.rguezb21@gmail.com>
2024-05-11 22:40:01 -0600

    creacion de sucursal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { PrestamosComponent } from './Prestamo/prestamos/prestamos.component';
 import { CreacionPrestamosComponent } from './Prestamo/creacion-prestamos/creacion-prestamos.component';
 import { VentasComponent } from './Venta/ventas/ventas.component';
 import { InventarioComponent } from './Invetarios/inventario/inventario.component';
-
+import { CreacionSucursalComponent } from './creacion-sucursal/creacion-sucursal.component';
 export const routes: Routes = [
     {path: 'prestamos', component:PrestamosComponent },
     {path: 'creacionPrestamos', component:CreacionPrestamosComponent},
     {path: 'inventario', component:InventarioComponent},
-    {path: 'ventas', component:VentasComponent}];
+    {path: 'ventas', component:VentasComponent},
+    {path: 'creacionSucursal', component: CreacionSucursalComponent}];
 
