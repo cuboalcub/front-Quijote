028fb78b5ec92ac246ca30eccffb933f1b0f2074
cuboalcub <fvera8192@gmail.com>
2024-05-12 11:12:54 -0600

    modificacion rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { PrestamosComponent } from './Prestamo/prestamos/prestamos.component';
-import { CreacionPrestamosComponent } from './Prestamo/creacion-prestamos/creacion-prestamos.component';
 import { VentasComponent } from './Venta/ventas/ventas.component';
+import { PrestamosComponent } from './Prestamo/prestamos/prestamos.component';
 import { InventarioComponent } from './Invetarios/inventario/inventario.component';
-import { RegistroEmpleadosComponent } from './registro-empleados/registro-empleados.component';
-
+import { RegistroEmpleadosComponent } from './Clientes/registro-empleados/registro-empleados.component';
 export const routes: Routes = [
+    {path:"inventario", component:InventarioComponent},
     {path: 'prestamos', component:PrestamosComponent },
-    {path: 'creacionPrestamos', component:CreacionPrestamosComponent},
-    {path: 'inventario', component:InventarioComponent},
-    {path: 'ventas', component:VentasComponent},
-    {path: 'registroEmpleados', component:RegistroEmpleadosComponent}];
+    {path: 'ventas', component:VentasComponent },
+    {path: 'registroempleados', component:RegistroEmpleadosComponent}];
 
