ea2edd7d51e7f98608e23bfaa291de83edb70ba2
ETE12 <emilianotoes12@gmail.com>
2024-05-11 23:02:43 -0600

    sucursales acabado

---
+++ src/app/sucursale/sucursale.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SucursaleComponent } from './sucursale.component';
+
+describe('SucursaleComponent', () => {
+  let component: SucursaleComponent;
+  let fixture: ComponentFixture<SucursaleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SucursaleComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SucursaleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sucursale/sucursale.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sucursale',
+  standalone: true,
+  imports: [],
+  templateUrl: './sucursale.component.html',
+  styleUrl: './sucursale.component.css'
+})
+export class SucursaleComponent {
+
+}
