4b1a9e584be0eb66e52c6651348ee0ef50a8e9ec
ETE12 <emilianotoes12@gmail.com>
2024-05-19 15:56:38 -0600

    creacionventa

---
+++ src/app/creacion-venta/creacion-venta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creacion-venta',
+  standalone: true,
+  imports: [],
+  templateUrl: './creacion-venta.component.html',
+  styleUrl: './creacion-venta.component.css'
+})
+export class CreacionVentaComponent {
+
+}
