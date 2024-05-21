bac23ff122772d9bccb38ba9027d5bba2106d6c1
ETE12 <emilianotoes12@gmail.com>
2024-05-21 08:17:52 -0600

    detallecompra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { EditorialesComponent } from './Editorial/editoriales/editoriales.compon
 import { SucursaleComponent } from './Sucursal/sucursale/sucursale.component';
 import { GenerosComponent } from './Genero/generos/generos.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { DetalleCompraComponent } from './detalle-compra/detalle-compra.component';
 
 export const routes: Routes = [
     {path:"inventario", component:InventarioComponent},
@@ -30,6 +31,7 @@ export const routes: Routes = [
     {path: 'cliente', component:ClienteComponent},
     {path: 'sucursales', component:SucursaleComponent},
     {path: 'generos', component:GenerosComponent},
-    {path: 'empleados', component:EmpleadosComponent}];
+    {path: 'empleados', component:EmpleadosComponent},
+    {path: 'detalleCompra', component:DetalleCompraComponent}];
 
 
