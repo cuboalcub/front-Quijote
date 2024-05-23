45338b458276d8f289c5c41ab4bfc7e1d9d374d4
cuboalcub <fvera8192@gmail.com>
2024-05-23 08:22:28 -0600

    invetaio update

--- src/app/Invetarios/alta-libros/alta-libros.component.ts
+++ src/app/Invetarios/alta-libros/alta-libros.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InvenatrioService } from '../../shared/service/invenatrio.service';
 @Component({
   selector: 'app-alta-libros',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './alta-libros.component.html',
   styleUrl: './alta-libros.component.css'
 })
 export class AltaLibrosComponent {
-
+constructor(private inventarioService: InvenatrioService) {}
+  nombre: string = '';
+  fecha: string = '';
+  cantidad: number = 0;
+  precio: number = 0;
+  genero: string = '';
+  onSubmit() {
+    console.log(this.nombre, this.fecha, this.cantidad, this.precio, this.genero);
+    
+  }
+  onCancel() {
+    this.resetForm();
+  }
+  resetForm() {
+    this.nombre = '';
+    this.fecha = '';
+    this.cantidad = 0;
+    this.precio = 0;
+    this.genero = '';
+  }
 }
