f531b300ff90bb41ea5f7cf4f585afc19dfa0b41
cuboalcub <fvera8192@gmail.com>
2024-05-23 18:52:06 -0600

    finish file routes

---
+++ src/environments/api_routes.ts
@@ -0,0 +1,54 @@
+import { Sucursal } from "../app/shared/models/sucursal";
+
+export const API_ROUTEs = {
+    baseurl: "",
+    inventario:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    cliente:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    editorial:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    empleado:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    genero:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    prestamos:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    sucursal:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    },
+    venta:{
+        table:"",
+        create:"",
+        update:"",
+        delete:""
+    }
+    
+}
