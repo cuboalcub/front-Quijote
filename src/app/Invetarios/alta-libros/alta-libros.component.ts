d2d449ab215bf9f3bfb8b67a34ab6c1fd8a1d3bb
cuboalcub <fvera8192@gmail.com>
2024-05-23 18:44:44 -0600

    api routes

--- src/app/Invetarios/alta-libros/alta-libros.component.ts
+++ src/app/Invetarios/alta-libros/alta-libros.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InvenatrioService } from '../../shared/service/invenatrio.service';
+import { GenerosService } from '../../shared/service/generos.service';
 @Component({
   selector: 'app-alta-libros',
   standalone: true,
@@ -11,6 +12,9 @@ import { InvenatrioService } from '../../shared/service/invenatrio.service';
 })
 export class AltaLibrosComponent {
 constructor(private inventarioService: InvenatrioService) {}
+
+  arr =["Drama","Terror","Comedia"]
+
   nombre: string = '';
   fecha: string = '';
   cantidad: number = 0;
