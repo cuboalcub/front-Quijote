19e950326ff4175bfb77b81c388e7ee01086cb3e
cuboalcub <fvera8192@gmail.com>
2024-05-30 11:35:30 -0600

    update

--- src/app/shared/service/editoriales.service.ts
+++ src/app/shared/service/editoriales.service.ts
@@ -1,34 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpParamsOptions } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Editorial } from '../models/editorial';
-import { LiberiaApiService } from './liberia-api.service';
-import { API_ROUTES } from '../../../environments/api_routes';
+  import { Injectable } from '@angular/core';
+  import { HttpClient,HttpHeaders } from '@angular/common/http';
+  import { Observable } from 'rxjs';
+  import { Editorial } from '../models/editorial';
+  import { LiberiaApiService } from './liberia-api.service';
+  import { API_ROUTES } from '../../../environments/api_routes';
 
-@Injectable({
-  providedIn: 'root'
-})
+  @Injectable({
+    providedIn: 'root'
+  })
 
-export class EditorialesService implements  LiberiaApiService<Editorial> {
-  
-  base = API_ROUTES.baseurl
-  constructor(private http: HttpClient) { }
+  export class EditorialesService implements  LiberiaApiService<Editorial> {
+    
+    base = API_ROUTES.baseurl
+    constructor(private http: HttpClient) { }
 
-  get(): Observable<Editorial[]> {
-    return this.http.get<Editorial[]>(`${this.base}${API_ROUTES.editoriales.get}`);
+    get(): Observable<Editorial[]> {
+      return this.http.get<Editorial[]>(`${this.base}${API_ROUTES.editoriales.get}`);
+    }
+
+    update(id: number, Editorial:Editorial): Observable<Editorial> {
+      return this.http.put<Editorial>(`${this.base}${API_ROUTES.editoriales.update}${id}`,Editorial);
+    }
+
+    post(editorial: any): Observable<any> {    
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+      return this.http.post<Editorial>(`${this.base}${API_ROUTES.editoriales.post}`, editorial, {headers});
+    }
+    delete(id: number): Observable<Editorial> {
+      return this.http.delete<Editorial>(`${this.base}${API_ROUTES.editoriales.delete}${id}`);
+    }
   }
-
-  update(id: number, Editorial:Editorial): Observable<Editorial> {
-    return this.http.put<Editorial>(`${this.base}${API_ROUTES.editoriales.update}${id}`,Editorial);
-  }
-
-  post(editorial: any): Observable<any> {    
-    return this.http.post<Editorial>(`${this.base}${API_ROUTES.editoriales.post}`, editorial);
-
-  }
-  delete(id: number): Observable<Editorial> {
-    return this.http.delete<Editorial>(`${this.base}${API_ROUTES.editoriales.delete}${id}`);
-  }
-}
