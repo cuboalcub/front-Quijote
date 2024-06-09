8c6c72bdb39b45fdb678804e57e948b9d0097638
FanysSs <estefania.rguezb21@gmail.com>
2024-06-09 14:32:19 -0600

    scroll creacion de ventas y prestamos

--- src/app/Prestamo/creacion-prestamos/creacion-prestamos.component.ts
+++ src/app/Prestamo/creacion-prestamos/creacion-prestamos.component.ts
@@ -14,11 +14,32 @@ export class CreacionPrestamosComponent {
   inventario = [
     { id: '1', nombre: 'Libro A', fechaPublicacion: '2020-01-01', idSucursal: '1', idGenero: '1', idEditorial: '1', precio: '100', existencias: '10' },
     { id: '2', nombre: 'Libro B', fechaPublicacion: '2019-05-15', idSucursal: '2', idGenero: '2', idEditorial: '2', precio: '150', existencias: '5' },
+    { id: '3', nombre: 'Libro C', fechaPublicacion: '2018-07-23', idSucursal: '3', idGenero: '3', idEditorial: '3', precio: '200', existencias: '8' },
+    { id: '1', nombre: 'Libro A', fechaPublicacion: '2020-01-01', idSucursal: '1', idGenero: '1', idEditorial: '1', precio: '100', existencias: '10' },
+    { id: '2', nombre: 'Libro B', fechaPublicacion: '2019-05-15', idSucursal: '2', idGenero: '2', idEditorial: '2', precio: '150', existencias: '5' },
+    { id: '3', nombre: 'Libro C', fechaPublicacion: '2018-07-23', idSucursal: '3', idGenero: '3', idEditorial: '3', precio: '200', existencias: '8' },
+    { id: '1', nombre: 'Libro A', fechaPublicacion: '2020-01-01', idSucursal: '1', idGenero: '1', idEditorial: '1', precio: '100', existencias: '10' },
+    { id: '2', nombre: 'Libro B', fechaPublicacion: '2019-05-15', idSucursal: '2', idGenero: '2', idEditorial: '2', precio: '150', existencias: '5' },
+    { id: '3', nombre: 'Libro C', fechaPublicacion: '2018-07-23', idSucursal: '3', idGenero: '3', idEditorial: '3', precio: '200', existencias: '8' },
+    { id: '1', nombre: 'Libro A', fechaPublicacion: '2020-01-01', idSucursal: '1', idGenero: '1', idEditorial: '1', precio: '100', existencias: '10' },
+    { id: '2', nombre: 'Libro B', fechaPublicacion: '2019-05-15', idSucursal: '2', idGenero: '2', idEditorial: '2', precio: '150', existencias: '5' },
     { id: '3', nombre: 'Libro C', fechaPublicacion: '2018-07-23', idSucursal: '3', idGenero: '3', idEditorial: '3', precio: '200', existencias: '8' }
   ];
 
   carrito = [
     { id: '1', nombre: 'Libro A', precio: '100', cantidad: '1' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
+    { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' },
     { id: '2', nombre: 'Libro B', precio: '150', cantidad: '2' }
   ];
 
